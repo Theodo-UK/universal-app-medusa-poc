56f536e0f2d5e951d65b72c9c6c7c49d6c66b498
Justin <57725347+juskek@users.noreply.github.com>
2024-03-19 17:59:50 +0000

    Feat/medusa admin (#1)
    * build: add medusa admin
    
    * Update .gitignore
    
    * build: yarn add @medusajs/ui-preset
    
    * Update medusa-config.js
    
    * Update package.json

--- apps/medusa-store/medusa-config.js
+++ apps/medusa-store/medusa-config.js
@@ -45,13 +45,25 @@ const plugins = [
     },
   },
   // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
-  // {
-  //   resolve: "@medusajs/admin",
-  //   /** @type {import('@medusajs/admin').PluginOptions} */
-  //   options: {
-  //     autoRebuild: true,
-  //   },
-  // },
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      serve: true,
+      autoRebuild: true,
+      path: "/app",
+      outDir: "build",
+      develop: {
+        open: true,
+        port: 7001,
+        logLevel: "error",
+        stats: "normal",
+        allowedHosts: "auto",
+        webSocketURL: undefined,
+      },
+
+    },
+  },
 ]
 
 const modules = {
